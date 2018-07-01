edda1480ef6181539bd453f4c0ce8ac3bbc76bf8
dingtao <594101547@qq.com>
2018-07-01 20:32:40 +0800

    用户登录和管理模块

--- src/main/java/com/mmall/dao/UserMapper.java
+++ src/main/java/com/mmall/dao/UserMapper.java
@@ -1,5 +1,6 @@
 package com.mmall.dao;
 
+import com.mmall.common.ServiceResponse;
 import com.mmall.pojo.User;
 import org.apache.ibatis.annotations.Param;
 
@@ -21,4 +22,15 @@ public interface UserMapper {
     int checkEmail(String email);
 
     User selectLogin(@Param("username") String username, @Param("password") String password);
+
+    String selectQuestionByUsername(String username);
+
+    int checkAnswer(@Param("username") String username,@Param("question") String question,@Param("answer") String answer);
+
+    int updatePasswordByUsername(@Param("username") String username,@Param("password")String password);
+
+    int checkPassword(@Param("password") String password,@Param("userId") Integer userId);
+
+    int checkEmailById(@Param("email")String email,@Param("userId")Integer userId);
+
 }
--- src/main/java/com/mmall/service/IUserService.java
+++ src/main/java/com/mmall/service/IUserService.java
@@ -9,6 +9,12 @@ import com.mmall.pojo.User;
 public interface IUserService {
     ServiceResponse<User> login(String username, String password);
     ServiceResponse<String> register(User user);
-    public ServiceResponse<String> checkVaild(String str,String type);
+    ServiceResponse<String> checkVaild(String str,String type);
+    ServiceResponse selectQuestion(String username);
+    ServiceResponse<String> checkAnswer(String username,String question,String answer);
+    ServiceResponse<String> forgetRestPassword(String username,String passwordNew,String forgetToken);
+    ServiceResponse<String> resetPassword(String passwordOld,String passwordNew,User user);
+    ServiceResponse<User> updateInformation(User user);
+    ServiceResponse<User> getInformation(Integer userId);
 }
 
